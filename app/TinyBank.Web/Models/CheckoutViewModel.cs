73b3781e5476d8b72bc671dd8fcb58462017053b
ΑΝΔΡΕΑΣ ΚΑΣΤΡΙΣΙΟΣ <KASTRISIOS.A@nbg.gr>
2021-04-16 10:08:35 +0300

    Checkout page

---
+++ app/TinyBank.Web/Models/CheckoutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyBank.Web.Models
+{
+    public class CheckoutViewModel
+    {
+        public Guid CardId { get; set; }
+        public int ExpiryMonth { get; set; }
+        public int ExpiryYear { get; set; }
+        public double Amount { get; set; }
+    }
+}
