a59caf299e6b50aa2817fbe06fc15d2d25e1ca73
ΑΝΔΡΕΑΣ ΚΑΣΤΡΙΣΙΟΣ <KASTRISIOS.A@nbg.gr>
2021-04-16 09:03:31 +0300

    Initial Commit

---
+++ app/TinyBank.Web/Controllers/CardController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TinyBank.Core.Implementation.Data;
+using TinyBank.Core.Model;
+using TinyBank.Core.Services;
+using TinyBank.Core.Services.Options;
+using TinyBank.Web.Extensions;
+using TinyBank.Web.Models;
+
+namespace TinyBank.Web.Controllers
+{
+    [Route("card")]
+    public class CardController : Controller
+    {
+        private readonly ICardService _cards;
+        private readonly ILogger<HomeController> _logger;
+        private readonly TinyBankDbContext _dbContext;
+
+        // Path: '/Card'
+        public CardController(
+            TinyBankDbContext dbContext,
+            ILogger<HomeController> logger,
+            ICardService cards)
+        {
+            _logger = logger;
+            _cards = cards;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TinyBank.Core.Implementation/Services/CardService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyBank.Core.Services;
+
+namespace TinyBank.Core.Implementation.Services
+{
+    public class CardService : ICardService
+    {
+        private readonly Data.TinyBankDbContext _dbContext;
+
+        public CardService(Data.TinyBankDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
